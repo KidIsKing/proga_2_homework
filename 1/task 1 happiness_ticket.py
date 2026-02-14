6f22d1de619bbcf8d1e1de522a0e68972447de57
YanaMasalova <anulamasalova@gmail.com>
2026-02-14 20:09:09 +0500

    add comments

--- 1/task 1 happiness_ticket.py
+++ 1/task 1 happiness_ticket.py
@@ -1,12 +1,17 @@
 def happiness_ticket(ticket):
     summ_start_numbers = 0
     summ_end_numbers = 0
+
     for symbol in range(len(ticket)):
+        # Первые три символа — левая половина
         if symbol < 3:
             summ_start_numbers += int(ticket[symbol])
+        # Остальные три символа — правая
         else:
             summ_end_numbers += int(ticket[symbol])
 
+    # Если разница сумм равна 1 (по модулю), то либо предыдущий,
+    # либо следующий билет — счастливый
     if abs(summ_start_numbers - summ_end_numbers) == 1:
         return True
     return False
@@ -18,3 +23,4 @@ print(happiness_ticket("511112"))  # Fasle
 print(happiness_ticket("111110"))  # True, так как 111111 счастливый
 print(happiness_ticket("333333"))  # Fasle, так как сам по себе счастливый
 print(happiness_ticket("141112"))  # Fasle
+print(happiness_ticket("123450"))  # Fasle

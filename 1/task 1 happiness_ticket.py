1bee57033cda4180cb7870420cc2f6570befe05f
KidIsKing <anulamasalova@gmail.com>
2026-02-15 22:56:36 +0500

    update comments in task 1

--- 1/task 1 happiness_ticket.py
+++ 1/task 1 happiness_ticket.py
@@ -20,7 +20,7 @@ def happiness_ticket(ticket):
 print(happiness_ticket("111112"))  # True, так как 111111 счастливый
 print(happiness_ticket("227535"))  # False
 print(happiness_ticket("511112"))  # Fasle
-print(happiness_ticket("111110"))  # True, так как 111111 счастливый
+print(happiness_ticket("211111"))  # True, так как 211112 счастливый
 print(happiness_ticket("333333"))  # Fasle, так как сам по себе счастливый
 print(happiness_ticket("141112"))  # Fasle
 print(happiness_ticket("123450"))  # Fasle
